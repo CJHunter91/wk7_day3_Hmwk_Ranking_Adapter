cfacdac6112c06d5d2f640ffc91d6ff131d4cc0c
Chris Hunter <orkem2@gmail.com>
2017-08-09 16:38:36 +0100

    added test to check wins in gladiator class:

--- app/src/test/java/com/codeclan/gladiators/GladiatorTest.java
+++ app/src/test/java/com/codeclan/gladiators/GladiatorTest.java
@@ -21,4 +21,9 @@ public class GladiatorTest {
     public void canGetName(){
         assertEquals("Maximus", gladiator.getName());
     }
+
+    @Test
+    public void canGetWins(){
+        assertEquals(10, gladiator.getWins());
+    }
 }
