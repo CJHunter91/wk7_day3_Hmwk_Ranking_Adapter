55dcc75969aeba8002991e738d5f5eafcc1c85ba
Chris Hunter <orkem2@gmail.com>
2017-08-09 16:54:54 +0100

    created gladiator list

--- app/src/main/java/com/codeclan/gladiators/TopGladiators.java
+++ app/src/main/java/com/codeclan/gladiators/TopGladiators.java
@@ -1,9 +1,29 @@
 package com.codeclan.gladiators;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 09/08/2017.
  */
 
 public class TopGladiators {
+    private ArrayList<Gladiator> topList;
+
+    public TopGladiators(){
+        topList = new ArrayList<>();
+        topList.add(new Gladiator("Spartacus", 50, "65BC"));
+        topList.add(new Gladiator("Flamma", 46, "32BC"));
+        topList.add(new Gladiator("Commudus", 43, "192AD"));
+        topList.add(new Gladiator("Crixus", 38, "68BC"));
+        topList.add(new Gladiator("Carpophorus", 32, "231AD"));
+        topList.add(new Gladiator("Marcus Attilius", 29, "234BC"));
+        topList.add(new Gladiator("Hermes", 23, "26BC"));
+        topList.add(new Gladiator("Spiculus", 16, "68AD"));
+        topList.add(new Gladiator("Tetraites", 8, "34BC"));
+        topList.add(new Gladiator("Spartacus", 3, "234BC"));
+        topList.add(new Gladiator("You", 0, "1991AD"));
+    }
+
+    
 
 }
